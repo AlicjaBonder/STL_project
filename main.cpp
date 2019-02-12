23ebf159411f70fcb28ce343739c1767737e985e
AlicjaBonder <alicjabonder@o2.pl>
2019-02-12 21:11:19 +0100

    changes in main

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@
 int main()
 {
 	Database db;
-    Student jan("Jan", "Kowalski", "12345678",'M', "Rybacka 14/5", 111111);
+    Student jan("Jan", "Kowalski", "12345678",Gender::Male, "Rybacka 14/5", 111111);
     //Student jan("Jan", "Kowalski", 166666);
     db.addStudent(jan);
-    Student ewa("Ewa", "Konieczna","23456789",'F',"Kielecka 34/2", 222222);
+    Student ewa("Ewa", "Konieczna","23456789",Gender::Female,"Kielecka 34/2", 222222);
     //Student adam("Adam", "Adamski", 222222);
     db.addStudent(ewa);
     db.sortByIndex();
